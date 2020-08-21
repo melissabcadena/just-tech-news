870c0242f5b7d69ea34212b61b2c7369816df8c7
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-08-21 11:22:45 -0500

    add helpers and tests

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
